39266a9bb3bcd550fd92b438b20e91fa3558c11d
Dizotoff <izotov.dima@gmail.com>
2017-09-24 14:44:35 +0300

    different users channel added

--- public/js/index.js
+++ public/js/index.js
@@ -2,14 +2,8 @@ var socket = io();
 
 socket.on('connect',function () {
 console.log('Connected to server');
-
-
-
 });
-socket.emit('createMessage', {
-  from: 'YOBA',
-  text: 'Eto ti?'
-});
+
 
 socket.on('disconnect', function () {
 console.log('Disconnect from server');
